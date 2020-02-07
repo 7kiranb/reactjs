7b7688d4519445cd569b393459bc0cf3e7a7b376
kiran <7kiran.b@gmil.com>
2020-02-07 12:19:21 +0530

    Updates

---
+++ precache-manifest.fe1a19febbaff5f91ef43153e2835f7d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc4765deef4278ebb6e4fe81913a6255",
+    "url": "/songs/index.html"
+  },
+  {
+    "revision": "b41223e1fccc6cb81f11",
+    "url": "/songs/static/js/2.c60f6907.chunk.js"
+  },
+  {
+    "revision": "38d1f7573e3f8e03f12894c14c5352ff",
+    "url": "/songs/static/js/2.c60f6907.chunk.js.LICENSE"
+  },
+  {
+    "revision": "36be0b3da8e72e7e3af0",
+    "url": "/songs/static/js/main.762b582d.chunk.js"
+  },
+  {
+    "revision": "fc8dcd007500c0fd04ce",
+    "url": "/songs/static/js/runtime-main.91e7c810.js"
+  }
+]);
